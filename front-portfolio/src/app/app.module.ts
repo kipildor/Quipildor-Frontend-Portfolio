a874d674e0fea300ff4fd100d9a01c1a0dbe37e9
kipildor <leandro_1980@live.com.ar>
2022-05-22 16:40:51 -0300

    Componente Encabezado - V1

--- front-portfolio/src/app/app.module.ts
+++ front-portfolio/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    //FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
