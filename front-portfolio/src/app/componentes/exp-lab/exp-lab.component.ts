cfc375576a9257f39046168a1e5850ca66eab9d5
kipildor <leandro_1980@live.com.ar>
2022-06-01 17:47:51 -0300

    Conectando Experiencia Laboral - V1

--- front-portfolio/src/app/componentes/exp-lab/exp-lab.component.ts
+++ front-portfolio/src/app/componentes/exp-lab/exp-lab.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ExpLab } from 'src/app/modelos/exp-lab';
+import { ExpLabService } from 'src/app/servicios/exp-lab.service';
 
 @Component({
   selector: 'app-exp-lab',
@@ -7,9 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpLabComponent implements OnInit {
 
-  constructor() { }
+  public listaExp : ExpLab[]=[];
+  //public todavia : string = "Hasta el momento...";
+
+  constructor(private router:Router, private serviExp:ExpLabService) { }
 
   ngOnInit(): void {
+    this.serviExp.listarExperiencias().subscribe(data=>{this.listaExp=data});
   }
 
 }
---
+++ front-portfolio/src/app/servicios/exp-lab.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ExpLab } from '../modelos/exp-lab';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpLabService {
+
+  expLabURL = 'http://localhost:8080/exp';
+
+  constructor(private http:HttpClient) { }
+
+  public listarExperiencias():Observable<ExpLab[]> {
+    return this.http.get<ExpLab[]>(this.expLabURL);
+  }
+}
