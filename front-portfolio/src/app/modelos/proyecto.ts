5737f635a17404c934323415dec1da9eadc83e25
kipildor <leandro_1980@live.com.ar>
2022-06-04 22:14:52 -0300

    Agregado e implementado el campo Tecnologia de la entidad Proyecto

--- front-portfolio/src/app/modelos/proyecto.ts
+++ front-portfolio/src/app/modelos/proyecto.ts
@@ -12,18 +12,21 @@ export class Proyecto {
 
     urlPropia:string;
 
+    tecnologia:string;
+
     // FK
     persona_id : number;
 
     //Constructores
     constructor(idProyecto:number, nombreProy:string, fechaProy:number, descripcion:string, urlGithub:string, urlPropia:string,
-                persona_id : number) {
+                tecnologia:string, persona_id : number) {
     this.idProyecto = idProyecto;
     this.nombreProy = nombreProy;
     this.fechaProy = fechaProy;
     this.descripcion = descripcion;
     this.urlGithub = urlGithub;
     this.urlPropia = urlPropia;
+    this.tecnologia = tecnologia;
 
     this.persona_id = persona_id;
     }
