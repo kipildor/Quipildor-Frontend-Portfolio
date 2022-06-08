2c1104d7664798bd5cfe42cce558f9d301641351
kipildor <leandro_1980@live.com.ar>
2022-06-08 12:39:43 -0300

    Formulario Login Modal de forma 3 - centrado y transparente

--- front-portfolio/src/app/componentes/encabezado/encabezado.component.ts
+++ front-portfolio/src/app/componentes/encabezado/encabezado.component.ts
@@ -10,16 +10,34 @@ import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class EncabezadoComponent implements OnInit {
 
-  modalSwitch:boolean = false;
-  closeResult: string;
+  //modalSwitch:boolean = false;
+  closeResult: string = '';
 
   constructor(private http:HttpClient, public modalServ:NgbModal) { }
 
   ngOnInit(): void {
   }
-
+/*
   openModal(){
     this.modalSwitch = true;
+  }
+*/
+  open(content) {
+    this.modalServ.open(content, {ariaLabelledBy: 'modal-basic-title', centered: true }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 
