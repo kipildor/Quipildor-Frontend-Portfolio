a61040b27ca7e00997be8300f091e7940f9a5861
kipildor <leandro_1980@live.com.ar>
2022-07-29 03:35:26 -0300

    Deploy - V3 - URL de auth.service

--- front-portfolio/src/app/jwt/service/auth.service.ts
+++ front-portfolio/src/app/jwt/service/auth.service.ts
@@ -4,13 +4,15 @@ import { Observable } from 'rxjs';
 import { JwtDTO } from '../models/jwt-dto';
 import { LoginUsuario } from '../models/login-usuario';
 import { NuevoUsuario } from '../models/nuevo-usuario';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  apiUrl:string = environment.apiBaseUrl;
 
-  authURL = 'http://localhost:8080/auth/';
+  authURL = this.apiUrl+'/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
